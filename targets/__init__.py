4d83e04e87fca305cc4dbadba20172cca426d270
EA264728 <elouan.argouarch@cea.fr>
2022-04-06 12:03:44 +0200

    changes

--- targets/__init__.py
+++ targets/__init__.py
@@ -1,3 +1,3 @@
-from .density_estimation_target import *
+from .density_estimation_target import Dimension1
 from .variational_inference_target import *
 from .conditional_density_estimation_target import *
