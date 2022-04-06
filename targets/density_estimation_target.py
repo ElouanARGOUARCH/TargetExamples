0fb170817c63d4178d1b6b02cd645d65fda2cbc5
EA264728 <elouan.argouarch@cea.fr>
2022-04-06 13:58:38 +0200

    changes

--- targets/density_estimation_target.py
+++ targets/density_estimation_target.py
@@ -21,12 +21,12 @@ class DensityEstimationTarget(nn.Module):
 
         if samples.shape[-1] >= 2:
             plt.figure(figsize=(10, 5))
-            plt.scatter(samples[:, 0], samples[:,1],color='red',alpha=0.6)
+            plt.scatter(samples[:,1], samples[:,2],color='red',alpha=0.6)
             plt.legend()
 
 class TwoCircles(DensityEstimationTarget):
     def __init__(self):
-        pass
+        super().__init__()
 
     def sample(self, num_samples):
         X, y = datasets.make_circles(num_samples, factor=0.5, noise=0.05)
@@ -35,7 +35,7 @@ class TwoCircles(DensityEstimationTarget):
 
 class Moons(DensityEstimationTarget):
     def __init__(self):
-        pass
+        super().__init__()
 
     def sample(self, num_samples):
         X, y = datasets.make_moons(num_samples, noise=0.05)
@@ -44,7 +44,7 @@ class Moons(DensityEstimationTarget):
 
 class SCurve(DensityEstimationTarget):
     def __init__(self):
-        pass
+        super().__init__()
 
     def sample(self, num_samples):
         X, t = datasets.make_s_curve(num_samples, noise=0.05)
@@ -53,6 +53,7 @@ class SCurve(DensityEstimationTarget):
 
 class Dimension1(DensityEstimationTarget):
     def __init__(self):
+        super().__init__()
         num_component = 6
         means = torch.tensor([[-0.25], [1.875], [4.125], [6.25], [-5.5], [-8.5]])
         covs = torch.tensor([[[1.]], [[.5]], [[.5]], [[2.]], [[1]], [[1]]])
@@ -66,6 +67,7 @@ class Dimension1(DensityEstimationTarget):
 
 class Orbits(DensityEstimationTarget):
     def __init__(self):
+        super().__init__()
         number_planets = 7
         covs_target = 0.04 * torch.eye(2).unsqueeze(0).repeat(number_planets, 1, 1)
         means_target = 2.5 * torch.view_as_real(
@@ -80,69 +82,9 @@ class Orbits(DensityEstimationTarget):
     def sample(self, num_samples):
         return self.mix_target.sample([num_samples])
 
-
-
-    def __init__(self, choice, num_samples):
-        self.choices = ['Two circles','Moons','S Curve', 'Dimension 1', 'Orbits']
-        self.choice = choice
-        assert self.choice in self.choices, "'" + choice + "'" + ' not implemented, please select from ' + str(
-            self.choices)
-        if choice == 'S Curve':
-            X, t = datasets.make_s_curve(num_samples, noise=0.05)
-            X = StandardScaler().fit_transform(X)
-            self.target_samples = torch.tensor(X[:,[0,2]]).float()
-
-        if choice == "Dimension 1":
-            num_component = 6
-            means = torch.tensor([[-0.25], [1.875], [4.125], [6.25], [-5.5],[-8.5]])
-            covs = torch.tensor([[[1.]], [[.5]], [[.5]], [[2.]], [[1]],[[1]]])
-            comp = torch.ones(num_component)
-            mvn_target = MultivariateNormal(means, covs)
-            cat = Categorical(comp / torch.sum(comp))
-            mix_target = MixtureSameFamily(cat, mvn_target)
-            self.target_samples = mix_target.sample([num_samples])
-
-        if choice == "Orbits":
-            number_planets = 7
-            covs_target = 0.04*torch.eye(2).unsqueeze(0).repeat(number_planets,1,1)
-            means_target = 2.5*torch.view_as_real(torch.pow(torch.exp(torch.tensor([2j * 3.1415 / number_planets])), torch.arange(0, number_planets)))
-            weights_target = torch.ones(number_planets)
-            weights_target = weights_target
-
-            mvn_target = MultivariateNormal(means_target, covs_target)
-            cat = Categorical(torch.exp(weights_target) / torch.sum(torch.exp(weights_target)))
-            mix_target = MixtureSameFamily(cat, mvn_target)
-            self.target_samples = mix_target.sample([num_samples])
-
-        if choice == "Banana":
-            self.var = 2
-            self.dim = 50
-            self.even = torch.arange(0,self.dim, 2)
-            self.odd = torch.arange(1,self.dim, 2)
-            self.mvn = torch.distributions.MultivariateNormal(torch.zeros(self.dim), self.var * torch.eye(self.dim))
-            x = self.mvn.sample([num_samples])
-            z = x.clone()
-            z[..., self.odd] += z[..., self.even] ** 2
-            self.target_samples = z
-
-
-    def get_samples(self):
-        return self.target_samples
-
-    def target_visual(self, num_samples = 5000):
-        num_samples = min(num_samples, self.target_samples.shape[0])
-        if self.target_samples.shape[-1] == 1:
-            plt.figure(figsize=(10, 5))
-            plt.hist(self.target_samples[:num_samples][:, 0].cpu().numpy(), bins=150, color='red',density = True, alpha=0.6,label=self.choice + " samples")
-            plt.legend()
-
-        if self.target_samples.shape[-1] >= 2:
-            plt.figure(figsize=(10, 5))
-            plt.scatter(self.target_samples[:, 0], self.target_samples[:,1],color='red',alpha=0.6,label=self.choice + " samples")
-            plt.legend()
-
 class Banana(DensityEstimationTarget):
     def __init__(self):
+        super().__init__()
         var = 2
         dim = 50
         self.even = torch.arange(0, dim, 2)
--- targets/variational_inference_target.py
+++ targets/variational_inference_target.py
@@ -1,110 +1,79 @@
 import torch
+from torch import nn
 from torch.distributions import MultivariateNormal, Categorical, MixtureSameFamily, Uniform
 import math
 
 
-class Uniform:
-    def __init__(self, lower, upper):
-        self.p = upper.shape[0]
-        self.lower = lower
-        self.upper = upper
-        assert torch.sum(upper>lower) == self.p, 'upper bound should be greater or equal to lower bound'
-        self.log_scale = torch.log(self.upper - self.lower)
-        self.location = (self.upper + self.lower)/2
+
+class VariationalInferenceTarget(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def log_prob(self):
+        raise NotImplementedError
+
+class Orbits(VariationalInferenceTarget):
+    def __init__(self):
+        super().__init__()
+        self.p = 2
+        number_planets = 7
+        covs_target = 0.04*torch.eye(self.p).unsqueeze(0).repeat(number_planets,1,1)
+        means_target = 2.5*torch.view_as_real(torch.pow(torch.exp(torch.tensor([2j * math.pi / number_planets])), torch.arange(0, number_planets)))
+        weights_target = torch.ones(number_planets)
+        weights_target = weights_target
+
+        mvn_target = MultivariateNormal(means_target, covs_target)
+        cat = Categorical(torch.exp(weights_target) / torch.sum(torch.exp(weights_target)))
+        self.mix_target = MixtureSameFamily(cat, mvn_target)
 
     def log_prob(self, samples):
-        condition = ((samples > self.lower).sum(-1) == self.p) * ((samples < self.upper).sum(-1) == self.p)*1
-        inverse_condition = torch.logical_not(condition) * 1
-        true = -torch.logsumexp(self.log_scale, dim = -1) * condition
-        false = torch.nan_to_num(-torch.inf*inverse_condition, nan = 0)
-        return (true + false)
+        return self.mix_target.log_prob(samples.cpu()).to(samples.device)
 
-    def sample(self, num_samples):
-        desired_size = num_samples.copy()
-        desired_size.append(self.p)
-        return self.lower.expand(desired_size) + torch.rand(desired_size)*torch.exp(self.log_scale.expand(desired_size))
+class Banana(VariationalInferenceTarget):
+    def __init__(self):
+        super().__init__()
+        var = 2
+        self.p = 50
+        self.even = torch.arange(0, self.p, 2)
+        self.odd = torch.arange(1, self.p, 2)
+        self.mvn = torch.distributions.MultivariateNormal(torch.zeros(self.p), var * torch.eye(self.p))
 
-class Mixture:
-    def __init__(self, distributions, weights):
-        self.distributions = distributions
-        self.number_components = len(self.distributions)
-        assert weights.shape[0] == self.number_components, 'wrong number of weights'
-        self.weights = weights/torch.sum(weights)
+    def inv_transform(self, z):
+        x = z.clone()
+        x[..., self.odd] -= x[..., self.even] ** 2
+
+        return x
 
     def log_prob(self, samples):
-        list_evaluated_distributions = []
-        for i,distribution in enumerate(self.distributions):
-            list_evaluated_distributions.append(distribution.log_prob(samples).reshape(samples.shape[:-1]).unsqueeze(1) + torch.log(self.weights[i]))
-        return(torch.logsumexp(torch.cat(list_evaluated_distributions, dim =1), dim = 1))
+        return self.mvn.log_prob(self.inv_transform(samples))
 
-    def sample(self, num_samples):
-        sampled_distributions = []
-        for distribution in self.distributions:
-            sampled_distributions.append(distribution.sample(num_samples).unsqueeze(1))
-        temp = torch.cat(sampled_distributions, dim = 1)
-        pick = Categorical(self.weights).sample(num_samples).squeeze(-1)
-        temp2 = torch.stack([temp[i,pick[i],:] for i in range(temp.shape[0])])
-        return temp2
+class Dimension1(VariationalInferenceTarget):
+    def __init__(self):
+        super().__init__()
+        self.p = 1
+        temp = torch.distributions.laplace.Laplace(torch.tensor([0.]),torch.tensor([2]))
+        uniform = Uniform(torch.tensor([-3]), torch.tensor([3]))
+        uniform2 = Uniform(torch.tensor([-5.0]), torch.tensor([5.0]))
+        self.mvn = Mixture([uniform,uniform2, temp], torch.tensor([1.,1., 1.]))
 
+    def log_prob(self, samples):
+        return self.mvn.log_prob(samples.cpu()).to(samples.devices)
 
-class VariationalInferenceTarget:
-    def __init__(self, choice):
-        self.choices = ["Sharp Edges", "Dimension 1", "Blob Dimension 64", "Orbits"]
-        self.choice = choice
-        assert self.choice in self.choices, "'" + choice + "'" + ' not implemented, please select from ' + str(
-            self.choices)
+class BlobDimension64(VariationalInferenceTarget):
+    def __init__(self):
+        super().__init__()
+        self.p = 64
+        mixtures_target = 4 + 2*torch.randint(0,6,[1])
+        L = torch.randn(mixtures_target, self.p, self.p)
+        covs_target = L @ L.transpose(-1, -2) + torch.eye(self.p)
+        covs_target = 2*covs_target
+        means_target = self.p*torch.randn(mixtures_target, self.p)/2
+        means_target = means_target
+        weights_target = torch.ones(mixtures_target)
+        weights_target = weights_target
+        mvn_target = MultivariateNormal(means_target, covs_target)
+        cat = Categorical(torch.exp(weights_target) / torch.sum(torch.exp(weights_target)))
+        self.mvn = MixtureSameFamily(cat, mvn_target)
 
-        if choice == "Sharp Edges":
-            self.p = 1
-            temp = torch.distributions.laplace.Laplace(torch.tensor([0.]),torch.tensor([2]))
-            uniform = Uniform(torch.tensor([-3]), torch.tensor([3]))
-            uniform2 = Uniform(torch.tensor([-5.0]), torch.tensor([5.0]))
-            target = Mixture([uniform,uniform2, temp], torch.tensor([1.,1., 1.]))
-            self.target_log_density = lambda samples: target.log_prob(samples.cpu()).to(samples.devices)
-
-        if choice == "Dimension 1":
-            self.p = 1
-            num_component = 6
-            means = torch.tensor([[-0.25], [1.875], [4.125], [6.25], [-5.5],[-8.5]])
-            covs = torch.tensor([[[1.]], [[.5]], [[.5]], [[2.]], [[1]],[[1]]])
-            comp = torch.ones(num_component)
-            mvn_target = MultivariateNormal(means, covs)
-            cat = Categorical(comp / torch.sum(comp))
-            mix_target = MixtureSameFamily(cat, mvn_target)
-            self.target_log_density = lambda samples: mix_target.log_prob(samples.cpu()).to(samples.device)
-
-        if choice == "Blob Dimension 64":
-            self.p = 64
-            mixtures_target = 4 + 2*torch.randint(0,6,[1])
-            L = torch.randn(mixtures_target, self.p, self.p)
-            covs_target = L @ L.transpose(-1, -2) + torch.eye(self.p)
-            covs_target = 2*covs_target
-            means_target = self.p*torch.randn(mixtures_target, self.p)/2
-            means_target = means_target
-            weights_target = torch.ones(mixtures_target)
-            weights_target = weights_target
-
-            mvn_target = MultivariateNormal(means_target, covs_target)
-            cat = Categorical(torch.exp(weights_target) / torch.sum(torch.exp(weights_target)))
-            mix_target = MixtureSameFamily(cat, mvn_target)
-            self.target_log_density = lambda samples: mix_target.log_prob(samples.cpu()).to(samples.device)
-
-        if choice == "Orbits":
-            self.p = 2
-            number_planets = 7
-            covs_target = 0.04*torch.eye(self.p).unsqueeze(0).repeat(number_planets,1,1)
-            means_target = 2.5*torch.view_as_real(torch.pow(torch.exp(torch.tensor([2j * math.pi / number_planets])), torch.arange(0, number_planets)))
-            weights_target = torch.ones(number_planets)
-            weights_target = weights_target
-
-            mvn_target = MultivariateNormal(means_target, covs_target)
-            cat = Categorical(torch.exp(weights_target) / torch.sum(torch.exp(weights_target)))
-            mix_target = MixtureSameFamily(cat, mvn_target)
-            self.target_log_density = lambda samples: mix_target.log_prob(samples.cpu()).to(samples.device)
-
-    def get_log_density(self):
-        return self.target_log_density, self.p
-
-    def target_visual(self):
-        return()
-
+    def log_prob(self, samples):
+        return self.mvn.log_prob(samples.cpu()).to(samples.device)
